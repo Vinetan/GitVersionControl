12d727d7c3befb9c30c901331cfe60849579eeff
Vinetan <vinetan@icloud.com>
2021-03-10 21:41:11 +0800

    Initial Commit

---
+++ Shared/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  Shared
+//
+//  Created by Vinetan on 2021/3/10.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
